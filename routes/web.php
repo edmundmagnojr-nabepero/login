69afccfb5931c2e0b22fd554612aa9678e20e7d8
edmundmagnojr-nabepero <edmund.magno@nabepero.co.jp>
2022-12-13 15:27:42 +0800

    finished Section 22

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+
+    // if(Auth::check()){
+    //     return "the user is logged in";
+    // }
+
+    // $username = 'sfjdslkfs';
+    // $password = 'fdkfdf';
+    // if(Auth::attempt(['username'=>$username, 'password'=>$password])){
+    //     return redirect()->intended();
+    // }
+
+    Auth::logout();
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
